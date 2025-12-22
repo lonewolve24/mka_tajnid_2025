1b5a5c18362bd55a4e925878540d3d5a90a0a1bd
Abdul Muizz <azikumapayi@gmail.com>
2025-12-22 15:52:52 +0000

    Refactor settings.py to utilize environment variables for SECRET_KEY, DEBUG, and ALLOWED_HOSTS, enhancing security and flexibility in production configurations.

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-v+q&1%incnnw8i1fss$oczi=_laxe8dhc7db#v+)(&j3-kcglc')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-v+q&1%incnnw8i1fss$oczi=_laxe8dhc7db#v+)(&j3-kcglc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = DEBUG = os.environ.get('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='', cast=Csv())
 
 
 # Application definition
